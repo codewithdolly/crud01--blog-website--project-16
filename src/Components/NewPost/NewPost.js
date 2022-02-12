7b7ec3e38d4b4d0ba0f40a6050cac8c040121de1
hicoderindia <hicoder.dolly@gmail.com>
2022-02-12 23:32:27 +0530

    New post -page created

--- src/Components/NewPost/NewPost.js
+++ src/Components/NewPost/NewPost.js
@@ -1,10 +1,100 @@
-import React from 'react'
+import React from "react";
 import "./NewPost.scss";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+
+const Input = styled("input")({
+  display: "none",
+});
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
-  )
-}
+    <>
+      <Box
+        className="newPost"
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 2 },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Typography
+          variant="h1"
+          component="div"
+          gutterBottom
+          sx={{ fontSize: "25px" }}
+        >
+          Create New Post <br />
+          -----------------------
+        </Typography>
 
-export default NewPost
+        {/* Header */}
+        <TextField
+          id="outlined-basic"
+          placeholder="Header"
+          variant="outlined"
+          id="newPost--header"
+        />
+        {/* Body */}
+        <Box>
+          <TextareaAutosize
+            aria-label="minimum height"
+            minRows={16}
+            placeholder="Type here..."
+            style={{
+              width: 500,
+              border: "1px solid silver",
+              borderRadius: "5px",
+            }}
+          />
+        </Box>
+
+        {/* Adding Images */}
+        <Box
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Avatar
+            variant="rounded"
+            style={{
+              border: "1px dashed silver",
+              width: "10rem",
+              height: "10rem",
+              marginRight: "10px",
+            }}
+          >
+            <PhotoCamera />
+          </Avatar>
+          <label htmlFor="contained-button-file">
+            <Input
+              accept="image/*"
+              id="contained-button-file"
+              multiple
+              type="file"
+            />
+            <Button variant="contained" color="inherit" component="span">
+              Upload
+            </Button>
+          </label>
+        </Box>
+
+        {/* Add post Button */}
+        <Button variant="contained" color="success" sx={{ mx: 4, px: 25 }}>
+          Add Post
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default NewPost;
