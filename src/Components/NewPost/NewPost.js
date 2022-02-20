ca02213fa65140d50cbb28b3da918e182855c8b0
hicoderindia <hicoder.dolly@gmail.com>
2022-02-20 19:01:39 +0530

    up

--- src/Components/NewPost/NewPost.js
+++ src/Components/NewPost/NewPost.js
@@ -1,32 +1,9 @@
-import React from "react";
-import "./NewPost.scss";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import TextareaAutosize from "@mui/material/TextareaAutosize";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import PhotoCamera from "@mui/icons-material/PhotoCamera";
-import { styled } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
-import Avatar from "@mui/material/Avatar";
-
-const Input = styled("input")({
-  display: "none",
-});
+import React from 'react'
 
 const NewPost = () => {
   return (
-    <>
+    <div>NewPost</div>
+  )
+}
 
-
-
-
-
-
-
-    
-    </>
-  );
-};
-
-export default NewPost;
+export default NewPost
---
+++ src/Components/ViewPost/singlePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const singlePage = () => {
+  return (
+    <div>singlePage</div>
+  )
+}
+
+export default singlePage
