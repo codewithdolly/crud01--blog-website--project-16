6c21e03054a88c4d99342f3fe9668ffe8830d29e
hicoderindia <hicoder.dolly@gmail.com>
2022-02-21 02:07:51 +0530

    logo added and little work done on home page

--- src/Components/ViewPost/ViewPost.js
+++ src/Components/ViewPost/ViewPost.js
@@ -1,17 +1,46 @@
-import React from 'react'
-import "./ViewPost.scss"
-import { Typography } from '@mui/material'
-import logo from "../images/logo.png"
+import React from "react";
+import "./ViewPost.scss";
+import { Typography } from "@mui/material";
+import logo from "../images/logo.png";
 
 const ViewPost = () => {
   return (
     <>
-      <div className='viewPost'>
-      <img src={logo} alt="logo" style={{marginTop:"100px"}} />
-      <Typography variant="h5" component="div"></Typography>
+      <div
+        className="viewPost"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Typography component="div">
+          <img src={logo} alt="logo" />
+        </Typography>
+        <Typography component="div" sx={{ mt: -5 }}>
+          <a href="#" style={{ textDecoration: "none", color: "black" }}>
+            <i class="fa-brands fa-facebook-f" style={{ padding: "0 5px" }} />
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "black" }}>
+            <i class="fa-brands fa-instagram" style={{ padding: "0 5px" }} />
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "black" }}>
+            {" "}
+            <i class="fa-brands fa-linkedin-in" style={{ padding: "0 5px" }} />
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "black" }}>
+            <i class="fa-brands fa-github" style={{ padding: "0 5px" }} />
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "black" }}>
+            <i
+              class="fa-solid fa-earth-americas"
+              style={{ padding: "0 5px" }}
+            />
+          </a>
+        </Typography>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ViewPost
+export default ViewPost;
