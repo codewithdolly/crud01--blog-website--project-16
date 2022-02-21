aa41c3bca0ad2443d15d440840a9d3d1c82f69ca
hicoderindia <hicoder.dolly@gmail.com>
2022-02-22 00:23:04 +0530

    up

---
+++ src/Components--/NewPost/NewPost.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./NewPost.scss";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+
+const Input = styled("input")({
+  display: "none",
+});
+
+const NewPost = () => {
+  return (
+    <>
+
+
+
+
+
+
+
+    
+    </>
+  );
+};
+
+export default NewPost;
--- src/Components/ViewPost/Home.js
+++ src/Components/ViewPost/Home.js
@@ -17,86 +17,121 @@ const Home = () => {
     <>
       <CssBaseline />
       <Box>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={12} md={12} lg={12} className="main-img">
-          <Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={12} lg={12} className="main-img">
+            <Typography>
+              <img
+                src={hillImg}
+                alt=""
+                width="100%"
+                style={{ opacity: ".6", borderRadius: "10px" }}
+              />
+            </Typography>
+            <Box className="main-img--text">
+              <Typography variant="h3" component="div">
+                AENEAN AC DIAM
+              </Typography>
+              <Typography variant="h5" component="div">
+                Let's <span>EXPLORE</span> the world!
+              </Typography>
+              <Button
+                variant="outlined"
+                size="large"
+                color="inherit"
+                sx={{ mt: 5, px: 3, fontWeight: "bold" }}
+              >
+                See More
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+
+      {/* second page */}
+      <Box>
+        <Grid container spacing={2}>
+          <Grid xs={12} sm={12} md={8} lg={8} sx={{ px: 2 }}>
+            <Card>
+              <CardMedia
+                component="img"
+                height="auto"
+                image={hillImg}
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom component="div">
+                  07 JUN 2021
+                </Typography>
+                <Typography gutterBottom variant="h4" component="div">
+                  MAECENAS CONSECTETUR
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  endIcon={<ArrowRightAltIcon />}
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                  ut enim ad minim veniam. Nunc maximus arcu sit amet accumsan
+                  imperdiet. Aliquam elementum efficitur ipsum nec blandit.
+                  Pellentesque posuere vitae metus sed auctor. Nullam accumsan
+                  fringilla ligula non pellentesque.{" "}
+                  <Button color="inherit">
+                    <ArrowRightAltIcon />
+                  </Button>
+                </Typography>
+              </CardContent>
+              <Box>
+                <CardActions>
+                  <Button size="small">Post By AD-THEME</Button>
+                </CardActions>
+                <Typography>
+                  {" "}
+                  <a href="#" style={{ textDecoration: "none", color: "gray" }}>
+                    <i
+                      class="fa-brands fa-facebook-f"
+                      style={{ padding: "0 5px" }}
+                    />
+                  </a>
+                  <a href="#" style={{ textDecoration: "none", color: "gray" }}>
+                    <i
+                      class="fa-brands fa-instagram"
+                      style={{ padding: "0 5px" }}
+                    />
+                  </a>
+                  <a href="#" style={{ textDecoration: "none", color: "gray" }}>
+                    {" "}
+                    <i
+                      class="fa-brands fa-linkedin-in"
+                      style={{ padding: "0 5px" }}
+                    />
+                  </a>
+                  <a href="#" style={{ textDecoration: "none", color: "gray" }}>
+                    <i
+                      class="fa-brands fa-github"
+                      style={{ padding: "0 5px" }}
+                    />
+                  </a>
+                  <a href="#" style={{ textDecoration: "none", color: "gray" }}>
+                    <i
+                      class="fa-solid fa-earth-americas"
+                      style={{ padding: "0 5px" }}
+                    />
+                  </a>
+                </Typography>
+              </Box>
+            </Card>
+          </Grid>
+          <Grid xs={12} sm={12} md={4} lg={4}>
             <img
               src={hillImg}
               alt=""
               width="100%"
               style={{ opacity: ".6", borderRadius: "10px" }}
             />
-          </Typography>
-          <Box className="main-img--text">
-            <Typography variant="h3" component="div">
-              AENEAN AC DIAM
-            </Typography>
-            <Typography variant="h5" component="div">
-              Let's <span>EXPLORE</span> the world!
-            </Typography>
-            <Button
-              variant="outlined"
-              size="large"
-              color="inherit"
-              sx={{ mt: 5, px: 3, fontWeight: "bold" }}
-            >
-              See More
-            </Button>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
       </Box>
-
-      {/* second page */}
-      <Box></Box>
-      <Grid container spacing={2}>
-        <Grid xs={12} sm={12} md={8} lg={8} sx={{ px: 2 }}>
-          <Card>
-            <CardMedia
-              component="img"
-              height="auto"
-              image={hillImg}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom component="div">
-                07 JUN 2021
-              </Typography>
-              <Typography gutterBottom variant="h4" component="div">
-                MAECENAS CONSECTETUR
-              </Typography>
-              <Typography
-                variant="body2"
-                color="text.secondary"
-                endIcon={<ArrowRightAltIcon />}
-              >
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua ut
-                enim ad minim veniam. Nunc maximus arcu sit amet accumsan
-                imperdiet. Aliquam elementum efficitur ipsum nec blandit.
-                Pellentesque posuere vitae metus sed auctor. Nullam accumsan
-                fringilla ligula non pellentesque.{" "}
-                <Button color="inherit">
-                  <ArrowRightAltIcon />
-                </Button>
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Post By AD-THEME</Button>
-              <Box></Box>
-            </CardActions>
-          </Card>
-          {/* <img src={hillImg} alt="" width="100%" style={{ opacity: ".6", borderRadius:"10px" }} /> */}
-        </Grid>
-        <Grid xs={12} sm={12} md={4} lg={4}>
-          <img
-            src={hillImg}
-            alt=""
-            width="100%"
-            style={{ opacity: ".6", borderRadius: "10px" }}
-          />
-        </Grid>
-      </Grid>
     </>
   );
 };
