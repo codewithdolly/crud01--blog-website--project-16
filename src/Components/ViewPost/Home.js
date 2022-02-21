721e96f837dd2ff5838b6a44e06b50d4c72a5891
hicoderindia <hicoder.dolly@gmail.com>
2022-02-21 18:25:18 +0530

    Update Home.js

--- src/Components/ViewPost/Home.js
+++ src/Components/ViewPost/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Home.scss"
+import "./Home.scss";
 import hillImg from "../images/hill.jpg";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
@@ -14,17 +14,26 @@ const Home = () => {
       <CssBaseline />
       <Grid container spacing={2}>
         <Grid item xs={12} sm={12} md={12} lg={12} className="main-img">
-        
-        <Typography><img src={hillImg} alt="" width="100%" style={{opacity: ".6"}} /></Typography>
-        <Box  className="main-img--text">
-        <Typography variant="h3" component='div'>AENEAN AC DIAM</Typography>
-        <Typography variant="h5" component='div'>Let's <span>EXPLORE</span> the world!</Typography>
-        <Button variant="outlined" size="large" color="inherit" sx={{mt: 5, px:3, fontWeight:"bold",}}>See More</Button>
-        </Box>
-        
-          
+          <Typography>
+            <img src={hillImg} alt="" width="100%" style={{ opacity: ".6" }} />
+          </Typography>
+          <Box className="main-img--text">
+            <Typography variant="h3" component="div">
+              AENEAN AC DIAM
+            </Typography>
+            <Typography variant="h5" component="div">
+              Let's <span>EXPLORE</span> the world!
+            </Typography>
+            <Button
+              variant="outlined"
+              size="large"
+              color="inherit"
+              sx={{ mt: 5, px: 3, fontWeight: "bold" }}
+            >
+              See More
+            </Button>
+          </Box>
         </Grid>
-        
       </Grid>
     </>
   );
