3e9796cb795ce194305a6dd03e63645d423c21e0
hicoderindia <hicoder.dolly@gmail.com>
2022-02-21 13:08:40 +0530

    up

--- src/Components/ViewPost/SubNav.js
+++ src/Components/ViewPost/SubNav.js
@@ -4,6 +4,10 @@ import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Blog from "./Blog";
+import Home from "./Home";
+import Contact from "./Contact"
+import AboutUs from "./AboutUs"
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -47,31 +51,34 @@ const SubNav = () => {
 
   return (
     <>
-      <Container maxWidth="lg" sx={{pt:8}}>
+      <Container maxWidth="lg" sx={{ pt: 8 }}>
         <Box sx={{ width: "100%" }}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <Tabs
               value={value}
               onChange={handleChange}
               aria-label="basic tabs example"
+              textColor="secondary[100]"
+              style={{ color: "#9C8156" }}
+              indicatorColor="secondery"
             >
               <Tab label="Home" {...a11yProps(0)} />
               <Tab label="Blog" {...a11yProps(1)} />
               <Tab label="About Us" {...a11yProps(2)} />
-              <Tab label="Contact " {...a11yProps(3)} />
+              <Tab label="Contact" {...a11yProps(3)} />
             </Tabs>
           </Box>
           <TabPanel value={value} index={0}>
-            Item One
+            <Home />
           </TabPanel>
           <TabPanel value={value} index={1}>
-            Item Two
+            <Blog />
           </TabPanel>
           <TabPanel value={value} index={2}>
-            Item Three
+            <AboutUs />
           </TabPanel>
           <TabPanel value={value} index={3}>
-            Item four
+            <Contact />
           </TabPanel>
         </Box>
       </Container>
