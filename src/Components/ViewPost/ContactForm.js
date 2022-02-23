8a86cc06dafaa4707be26b72e843efc7626364ff
hicoderindia <hicoder.dolly@gmail.com>
2022-02-23 18:02:32 +0530

    Form Completed

--- src/Components/ViewPost/ContactForm.js
+++ src/Components/ViewPost/ContactForm.js
@@ -1,41 +1,54 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Button, Typography } from "@mui/material";
+import { Button, Card, Typography } from "@mui/material";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
 
 const ContactForm = () => {
   return (
-    <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { m: 4, width: "25ch" },
-      }}
+    <Box style={{padding:"10px", margin:"5px",  border:"2px solid menu", borderRadius:"10px"}}
+     component="form"
       noValidate
-      autoComplete="off"
-    >
-     <Typography>
-       {inputs.map((input)=>{
-          return (<>
-            <TextField
-          id="standard-helperText"
-          placeholder={input.placeholder}
-          helperText={input.helperText}
-          fullWidth
-          size="small"
-        />
-          </>)
-        })}</Typography>
-      <TextareaAutosize
+      autoComplete="off">
+      <Typography>
+        {inputs.map((input) => {
+          return (
+            <>
+              <TextField
+                id="standard-helperText"
+                placeholder={input.placeholder}
+                helperText={input.helperText}
+                fullWidth
+                size="small"
+                sx={{ my: 1 }}
+              />
+            </>
+          );
+        })}
+      </Typography>
+      <Typography>
+        {" "}
+        <TextareaAutosize
           aria-label="minimum height"
           minRows={10}
           placeholder="Type here..."
-          style={{ width: "90%", border:"1px solid silver", margin:"0 20px" }}
-         />
-    
-    <Button variant="contained" style={{backgroundColor:"#B19484", textTransform: "capitalize"}}>Send</Button>
+          style={{
+            width: "100%",
+            border: "1px solid silver",
+            alignContent: "center",
+            alignItems: "center",
+          }}
+        />
+      </Typography>
+      <Typography sx={{ my: 2 }}>
+        <Button
+          variant="contained"
+          style={{ backgroundColor: "#B19484", textTransform: "capitalize" }}
+        >
+          Send
+        </Button>
+      </Typography>
     </Box>
-    
   );
 };
 export default ContactForm;
